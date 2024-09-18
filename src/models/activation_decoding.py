4586d546f4465e9fc3d1d9aeddd12ced8b24edac
Aryo Gema <aryopg@gmail.com>
2024-09-18 11:46:13 +0100

    fix bug by taking the first entropy from the list

--- src/models/activation_decoding.py
+++ src/models/activation_decoding.py
@@ -116,8 +116,6 @@ class ActivationDecoding(BaseModel):
                 # we compute the adjust_score to calibrate the original score
                 adjust_score = None
 
-                print("logits: ", logits)
-                print("entropy: ", entropy)
                 if (
                     self.decoding_strategy == "entropy"
                     or self.decoding_mode == "activation_dola"
@@ -128,9 +126,8 @@ class ActivationDecoding(BaseModel):
                         logits = logits
 
                     adjust_score = -entropy
-                print("logits: ", logits)
 
-                entropies += [adjust_score.item()]
+                entropies += [adjust_score[0][0].item()]
                 past_kv = outputs.past_key_values
                 last_input_token = logits[0, -1].argmax()
                 generated_ids.append(last_input_token.item())
