f6c1503c42c0365ab4713f429634a3bb7e5e6eaf
Aryo Gema <aryopg@gmail.com>
2024-09-19 15:50:18 +0100

    return metrics

--- src/metrics/ifeval/ifeval.py
+++ src/metrics/ifeval/ifeval.py
@@ -11,11 +11,28 @@ class IFEval:
         pass
 
     def __call__(self, predictions) -> Dict[str, float]:
-        # TODO: process_results
-        doc = {
-            "key": predictions["key"],
-            "instruction_id_list": predictions["instruction_id_list"],
-            "prompt": predictions["prompt"],
-            "kwargs": predictions["kwargs"],
+        prompt_level_strict_accs = []
+        inst_level_strict_accs = []
+        prompt_level_loose_accs = []
+        inst_level_loose_accs = []
+        for prediction in predictions:
+            doc = {
+                "key": prediction["key"],
+                "instruction_id_list": prediction["instruction_id_list"],
+                "prompt": prediction["prompt"],
+                "kwargs": prediction["kwargs"],
+            }
+            metric = process_results(doc, prediction["predicted_answer"])
+
+            prompt_level_strict_accs.append(metric["prompt_level_strict_accs"])
+            inst_level_strict_accs.append(metric["inst_level_strict_accs"])
+            prompt_level_loose_accs.append(metric["prompt_level_loose_accs"])
+            inst_level_loose_accs.append(metric["inst_level_loose_accs"])
+
+        metrics = {
+            "prompt_level_strict_acc": np.mean(prompt_level_strict_accs),
+            "inst_level_strict_acc": np.mean(inst_level_strict_accs),
+            "prompt_level_loose_acc": np.mean(prompt_level_loose_accs),
+            "inst_level_loose_acc": np.mean(inst_level_loose_accs),
         }
-        process_results(doc, predictions["predicted_answer"])
+        return metrics
