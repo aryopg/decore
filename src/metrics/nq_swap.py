b37634eecdd734f19da67f25ff26c21fc4f454a8
aryopg@gmail.com <aryopg@gmail.com>
2024-12-17 14:37:58 +0000

    fix affected metrics

--- src/metrics/nq_swap.py
+++ src/metrics/nq_swap.py
@@ -1,7 +1,7 @@
 from typing import Dict, List
 
-import regex as re
 import numpy as np
+import regex as re
 
 from src.metrics.utils import best_em, best_subspan_em
 
@@ -37,7 +37,7 @@ class NQSwap:
             ]
 
             # Only consider until \n, ., or ,
-            prediction = re.split("\n|\.|\,", sample["predicted_answer"])[0]
+            prediction = re.split("\n", sample["predicted_answer"])[0]
 
             scores = self.compute_metrics(prediction, sub_ref, org_ref)
 
